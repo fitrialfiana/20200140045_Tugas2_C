acdb1d3c0f256965fc89233d56c8b6b6b1e94918
fitri alfiana <fitrialfiana62@gmail.com>
2022-03-25 22:55:40 +0700

    fungsi insert data

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace _20200140045_Tugas2_C
 {
@@ -10,6 +11,12 @@ namespace _20200140045_Tugas2_C
     {
         static void Main(string[] args)
         {
+            new Program().InsertData();
+        }
+
+        public void InsertData()
+        {
+
         }
     }
 }
