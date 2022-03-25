6cef73557343e88ba1e023b7e84e232b52c49640
fitri alfiana <fitrialfiana62@gmail.com>
2022-03-25 23:18:53 +0700

    menambah 5 data di tabel barang

--- Program.cs
+++ Program.cs
@@ -22,7 +22,11 @@ namespace _20200140045_Tugas2_C
                 con = new SqlConnection("data source=DESKTOP-6IF85PV\\FITRIALFIANA;database=FitriAlfiana;Integrated Security = TRUE");
                 con.Open();
 
-                SqlCommand cm = new SqlCommand("insert into Tabel_Barang (Kode_Kue, Nama_Kue, Harga_Kue, Tgl_Exp) values (1123, 'Lemper', 5000, '2022-03-28')", con);
+                SqlCommand cm = new SqlCommand("insert into Tabel_Barang (Kode_Kue, Nama_Kue, Harga_Kue, Tgl_Exp) values (1130, 'Putri Salju', 2000, '2022-04-28')" +
+                    "insert into Tabel_Barang (Kode_Kue, Nama_Kue, Harga_Kue, Tgl_Exp) values (1123, 'Lemper', 5000, '2022-03-28')" +
+                    "insert into Tabel_Barang (Kode_Kue, Nama_Kue, Harga_Kue, Tgl_Exp) values (1124, 'Donat', 15000, '2022-03-29')" +
+                    "insert into Tabel_Barang (Kode_Kue, Nama_Kue, Harga_Kue, Tgl_Exp) values (1126, 'Bolu kukus', 20000, '2022-03-27')" +
+                    "insert into Tabel_Barang (Kode_Kue, Nama_Kue, Harga_Kue, Tgl_Exp) values (1127, 'Kue semut', 10000, '2022-03-28')", con);
                 cm.ExecuteNonQuery();
 
                 Console.WriteLine("Sukses menambahkan data");
