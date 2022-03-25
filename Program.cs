065803d773150f21f4192607543b420fe1137a1e
fitri alfiana <fitrialfiana62@gmail.com>
2022-03-25 23:35:11 +0700

    tabel customer dan kasir

--- Program.cs
+++ Program.cs
@@ -27,7 +27,27 @@ namespace _20200140045_Tugas2_C
                     "insert into Tabel_Barang (Kode_Kue, Nama_Kue, Harga_Kue, Tgl_Exp) values (1124, 'Donat', 15000, '2022-03-29')" +
                     "insert into Tabel_Barang (Kode_Kue, Nama_Kue, Harga_Kue, Tgl_Exp) values (1126, 'Bolu kukus', 20000, '2022-03-27')" +
                     "insert into Tabel_Barang (Kode_Kue, Nama_Kue, Harga_Kue, Tgl_Exp) values (1127, 'Kue semut', 10000, '2022-03-28')", con);
+
+
                 cm.ExecuteNonQuery();
+
+                SqlCommand cmm = new SqlCommand("Insert into Tabel_Customer (Id_Customer, Nama_Customer, Alamat_Customer, No_Tlp) values ('A001', 'Ana', 'Jl. Rajawali', 088236782910)" +
+                    "Insert into Tabel_Customer (Id_Customer, Nama_Customer, Alamat_Customer, No_Tlp) values ('A003','Ardian','Jl. Mawar, Bulukumba',088283928484)" +
+                    "Insert into Tabel_Customer (Id_Customer, Nama_Customer, Alamat_Customer, No_Tlp) values ('A002','Anandi','Jl. Sabar',0828383847910)" +
+                    "Insert into Tabel_Customer (Id_Customer, Nama_Customer, Alamat_Customer, No_Tlp) values ('A004','Asani','Jl. Melati',089302374848)" +
+                    "Insert into Tabel_Customer (Id_Customer, Nama_Customer, Alamat_Customer, No_Tlp) values ('A005','Aina','Jl. Sultan Agung',085256782292)", con);
+
+                cmm.ExecuteNonQuery();
+
+                SqlCommand cm3 = new SqlCommand("Insert into Tabel_Kasir (Kode_Kasir, Nama_Kasir, Alamat_Kasir, No_Tlp) values ('000011', 'Diana', 'Jl. Baruga', 089728372837)" +
+                    "Insert into Tabel_Kasir (Kode_Kasir, Nama_Kasir, Alamat_Kasir, No_Tlp) values ('000012','Ardan','Jl. Anggrek, Bulukumba',082563894102)" +
+                    "Insert into Tabel_Kasir (Kode_Kasir, Nama_Kasir, Alamat_Kasir, No_Tlp) values ('000013','Esty','Jl. Kenanga, Makassar',085479210882)" +
+                    "Insert into Tabel_Kasir (Kode_Kasir, Nama_Kasir, Alamat_Kasir, No_Tlp) values ('000014','Jihan','Jl. Adiatsa',088990912448)" +
+                    "Insert into Tabel_Kasir (Kode_Kasir, Nama_Kasir, Alamat_Kasir, No_Tlp) values ('000015','Asasi','Jl. Sultan Hasanuddin',082930281927)", con);
+
+                cm3.ExecuteNonQuery();
+
+
 
                 Console.WriteLine("Sukses menambahkan data");
                 Console.ReadKey();
